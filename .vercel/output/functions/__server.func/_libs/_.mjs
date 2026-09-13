d359d2a860f5aef624fede16720226eaa853f0c2
Gabrieldeveloper1 <macielsilva1983@gmail.com>
2026-09-13 04:16:32 -0300

    Lynko Marketplace - nova versão

--- .vercel/output/functions/__server.func/_libs/_.mjs
+++
@@ -1,2 +0,0 @@
-import { rt as profile_functions_exports } from "../_ssr/router-BVA3mZO7.mjs";
-export { profile_functions_exports as n };
--- .vercel/output/static/assets/upload-BLR5Z409.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./utils-DA4HU7wP.js";var t=e(`clock-3`,[[`circle`,{cx:`12`,cy:`12`,r:`10`,key:`1mglay`}],[`path`,{d:`M12 6v6h4`,key:`135r8i`}]]),n=e(`upload`,[[`path`,{d:`M12 3v12`,key:`1x0j5s`}],[`path`,{d:`m17 8-5-5-5 5`,key:`7q97r8`}],[`path`,{d:`M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4`,key:`ih7n3h`}]]);export{t as n,n as t};
